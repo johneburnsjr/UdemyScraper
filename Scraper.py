750c4724cf11a75af63454a0de02405242c05412
John Burns <johnisjobu@gmail.com>
2020-06-11 20:37:51 -0400

    added logic and search more than one page

--- Scraper.py
+++ Scraper.py
@@ -3,9 +3,29 @@ import urllib.request
 from IPython.display import HTML
 import re
 
-r = urllib.request.urlopen('https://udemycoupon.learnviral.com/coupon-category/free100-discount/').read()
+userSearch = input("Type something to test this out: ")
+searchUrl = "https://udemycoupon.learnviral.com/?s="
+
+if userSearch != '':
+    r = urllib.request.urlopen(searchUrl + userSearch).read()
+else:    
+    r = urllib.request.urlopen('https://udemycoupon.learnviral.com/coupon-category/free100-discount/').read()
+# multiple clicks r = urllib.request.urlopen('https://www.discudemy.com/category/certification').read()
+# multiple clicks r = urllib.request.urlopen('https://udemycoupons.me/').read()
+
 
 soup = BeautifulSoup(r, "lxml")
+pages = soup.find('div', class_='pages')
+url = "https://udemycoupon.learnviral.com/coupon-category/free100-discount/page/"
+links = pages.find_all('a', class_='page-numbers')
 
-for link in soup.find_all('a', class_="coupon-code-link btn promotion"):
-    print(link.get('href'))
+count = 1
+for link in links:
+    count = count + 1
+    newurls = url + str(count)
+    print(newurls)
+    for site in soup.find_all('a',class_="coupon-code-link btn promotion"):
+        if userSearch != '':
+            print(site.get('href'))
+        else:
+            print(site.get('href'))      
